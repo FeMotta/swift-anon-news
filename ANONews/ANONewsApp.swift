5501854766bc8c7074430720e594849e93ed2f69
Fernando Motta <fernandosantosmotta@gmail.com>
2022-07-25 08:39:26 -0300

    Initial Commit

---
+++ ANONews/ANONewsApp.swift
@@ -0,0 +1,17 @@
+//
+//  ANONewsApp.swift
+//  ANONews
+//
+//  Created by Fernando dos Santos Motta on 25/07/22.
+//
+
+import SwiftUI
+
+@main
+struct ANONewsApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ ANONews/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  ANONews
+//
+//  Created by Fernando dos Santos Motta on 25/07/22.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, world!")
+            .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
